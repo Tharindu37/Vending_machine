65fa0497d625304e7701c93a808e268c121f123c
Tharindu37 <tharindut520@gmail.com>
2021-10-09 12:58:48 -0700

    Print Settings Punction_Update-0 and main method Update

--- main.c
+++ main.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-void printItem(char itemArray[20][20],int numberOfItem[20]);
+int printItem(char itemArray[20][20],int numberOfItem[20]);
+int printSettings();
 int arraySize=5;
 int main(){
-    char itemArray[20][20]={"Water Botties","Coca cola","Soda","Sprite","Pepsi"};//item array
+    char itemArray[20][20]={"Water Bottles","Coca cola","Soda","Sprite","Pepsi"};//item array
     int numberOfOneItem[20]={5,10,0,10,2};//number of one item array
     float priceOfOneItem[20]={100,70,150,80,90};//price of one item array
-    printItem(itemArray,numberOfOneItem);//call print item function
+    int chooseItem=printItem(itemArray,numberOfOneItem);//call print item function
+    int chooseSettings=0;
+    if(chooseItem==-1){
+        system("CLS");
+        chooseSettings=printSettings();
+    }else{
+
+    }
     return 0;
 }
 //Item print and get input.if input is -1 go settings
-void printItem(char itemArray[20][20],int numberOfItem[20]){
+int printItem(char itemArray[20][20],int numberOfItem[20]){
     int chooseItem=0;
     for(int i=0;i<arraySize;i++){
         if(numberOfItem[i]!=0){
@@ -19,5 +27,18 @@ void printItem(char itemArray[20][20],int numberOfItem[20]){
     }
     printf("<>Select the Item You Want? :");
     scanf("%d",&chooseItem);
+    return chooseItem;
 }
+//--->if Input -1
+int printSettings(){
+    int chooseSettings=0;
+    char settingsArray[6][50]={"Add More Items","Change The Price","The Number Of Items Available","Looking At The Profit","Increasing The Number Of Items","Exit"};
+    for(int i=0;i<6;i++){
+        printf("%d. %s\n",i+1,settingsArray[i]);
+    }
+    printf("<>Choose :");
+    scanf("%d",&chooseSettings);
+    return chooseSettings;
+}
+
 
